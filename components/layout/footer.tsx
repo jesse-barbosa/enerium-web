2480fea5e5d25f02e93380cb89182623b7d9f429
jesse-barbosa <barbosajesse419@gmail.com>
2025-07-10 22:08:44 -0300

    fix Copyright year in Footer: 2024 -> 2025

--- components/layout/footer.tsx
+++ components/layout/footer.tsx
@@ -76,7 +76,7 @@ export function Footer({ locale = "pt" }: FooterProps) {
         </div>
 
         <div className="border-t border-gray-800 mt-8 pt-8 text-center text-gray-400">
-          <p>&copy; 2024 Enerium. {t("allRightsReserved")}</p>
+          <p>&copy; 2025 Enerium. {t("allRightsReserved")}</p>
         </div>
       </div>
     </footer>
